cd3b2157b56c8d128a5cf6eb15a4afc98a81bae4
Gabriel <Gabriel@LAPTOP-P2KJ6ET1>
2023-06-09 18:18:48 -0300

    seatType now uses a enum constant

--- Buser/src/application/Ticket.java
+++ Buser/src/application/Ticket.java
@@ -1,40 +1,47 @@
 package application;
 
 public class Ticket {
-	private float price;			
-	private String seatType;	
-	private String seatNumber;		
+	private float price;		
+	private SeatType seatType;
+	private String seatNumber;
 	public Itinerary itinerary;
+	
+	enum SeatType {
+		executivo, semiLeito, leito
+	}
 
-	public Ticket(float price, String seatType, String seatNumber, Itinerary itinerary) {
-		this.setPrice(price);
+	public Ticket(float price, SeatType seatType, String seatNumber, Itinerary itinerary) {
+		this.set_Price(price, seatType);
 		this.setSeatType(seatType);
 		this.setSeatNumber(seatNumber);
 		this.setItinerary(itinerary);
-	}
-
-	public String toString() {
-		String saida;
-		saida = "Preço: 		   R$" + this.getPrice();
-		saida = "Tipo de poltrona: 	 " + saida + this.getSeatType();
-		saida = "Número da poltrona: " + saida + this.getSeatNumber();
-		saida = saida + this.getItinerary().toString();
-		return saida;
 	}
 	
 	public float getPrice() {
 		return price;
 	}
 	
+	private void set_Price(float value, SeatType seat) {
+		
+		if (seat == SeatType.executivo) {
+			this.price = value + 10;
+		}
+		if (seat == SeatType.semiLeito) {
+			this.price = value + 15;
+		}
+		if (seat == SeatType.leito) {
+			this.price = value + 20;
+		}
+	}
+	
 	public void setPrice(float value) {
 		this.price = value;
 	}
-	
-	public String getSeatType() {
+	public SeatType getSeatType() {
 		return seatType;
 	}
 	
-	public void setSeatType(String seatType) {
+	public void setSeatType(SeatType seatType) {
 		this.seatType = seatType;
 	}
 	
@@ -52,5 +59,14 @@ public class Ticket {
 	
 	public void setItinerary(Itinerary itinerary) {
 		this.itinerary = itinerary;
+	}
+	
+	public String toString() {
+		String saida;
+		saida = "Preço: 		   R$" + this.getPrice();
+		saida = "Tipo de poltrona: 	 " + saida + this.getSeatType();
+		saida = "Número da poltrona: " + saida + this.getSeatNumber();
+		saida = saida + this.getItinerary().toString();
+		return saida;
 	}
 }

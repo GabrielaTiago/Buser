c740da4599c465e4f834fb338369c76603826041
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-06-20 10:55:34 -0300

    Removes the "toString" method from the Itinerary

--- Buser/src/models/Itinerary.java
+++ Buser/src/models/Itinerary.java
@@ -1,8 +1,8 @@
-package application;
+package models;
 
 public class Itinerary {
 	private static int nextId = 1;
-	
+
 	private int id;
 	private String origin;
 	private String destination;
@@ -10,8 +10,9 @@ public class Itinerary {
 	private String departureDate;
 	private String arrivalDate;
 	private Company company;
-	
-	public Itinerary(String origin, String destination, String day, String departureDate, String arrivalDate, Company company) {
+
+	public Itinerary(String origin, String destination, String day, String departureDate, String arrivalDate,
+			Company company) {
 		this.setId();
 		this.setOrigin(origin);
 		this.setDestination(destination);
@@ -20,71 +21,61 @@ public class Itinerary {
 		this.setArrivalDate(arrivalDate);
 		this.setCompany(company);
 	}
-	
+
 	public void setId() {
 		this.id = nextId;
 		nextId++;
-	} 
-	
+	}
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setOrigin(String origin) {
 		this.origin = origin;
-	} 
-	
+	}
+
 	public String getOrigin() {
 		return origin;
 	}
-	
+
 	public void setDestination(String destination) {
 		this.destination = destination;
-	} 
-	
+	}
+
 	public String getDestination() {
 		return destination;
 	}
-	
+
 	public void setDay(String day) {
 		this.day = day;
-	} 
-	
+	}
+
 	public String getDay() {
 		return day;
-	}	
-	
+	}
+
 	public void setDepartureDate(String departureDate) {
 		this.departureDate = departureDate;
-	} 
-	
+	}
+
 	public String getDepartureDate() {
 		return departureDate;
 	}
-	
+
 	public void setArrivalDate(String arrivalDate) {
 		this.arrivalDate = arrivalDate;
-	} 
-	
+	}
+
 	public String getArrivalDate() {
 		return arrivalDate;
 	}
-	
+
 	public void setCompany(Company company) {
 		this.company = company;
 	}
-	
+
 	public Company getCompany() {
 		return company;
-	}
-	public String toString() {
-		String saida;
-		saida = "Empresa: 		" + this.getCompany().getName() 				+ "\n";
-		saida = "Data de chegada:  	" + this.getArrivalDate() 	+ "\n" + saida;
-		saida = "Data de embarque: 	" + this.getDepartureDate() + "\n" + saida;
-		saida = "Destino:    	   	" + this.getDestination() 		 	+ "\n" + saida;
-		saida = "Origem:    	   	" + this.getOrigin() 		 		+ "\n" + saida;
-
-		return saida;
 	}
 }

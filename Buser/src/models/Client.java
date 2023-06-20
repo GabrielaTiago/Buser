af538f4b210af3cda580f2d1f09fee2204a9f7f7
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-06-20 11:17:46 -0300

    Adds new constructor and attribute to the Client model

--- Buser/src/models/Client.java
+++ Buser/src/models/Client.java
@@ -1,82 +1,69 @@
-package application;
+package models;
 
 import java.util.ArrayList;
 
 public class Client extends User {
-	
 	private String cpf;
-	private gratuity gratuityType;
-	private gratuityDocument gratuityDocument; 
+	private String gratuity;
+	private GratuityType gratuityType;
+	private GratuityDocument gratuityDocument;
 	private ArrayList<Ticket> tickets;
-	
-	public enum gratuity{
-		no_gratuity, elderly, phisicallyChallenged
-	}
-	public enum gratuityDocument {
-		birthDate, freePass
-	}
-	
-	public Client(String name, String phone, String email, String address, String cpf,
-			ArrayList<Ticket> tickets, gratuity gratuityType) {
-		
-		super(name, phone, email, address);
-		
-		this.setCpf(cpf);
-		this.setTickets(tickets); 
-		
-		//checking gratuity category the client applies to
-		//and applying it to all existing tickets if so
-		if(gratuityType == gratuity.elderly) {
-			this.setGratuityType(gratuityType);
-			this.setGratuityDocument(gratuityDocument.birthDate);
-			this.applyGratuity();
-		}
-		else if(gratuityType == gratuity.phisicallyChallenged) {
-					this.setGratuityType(gratuityType);
-					this.setGratuityDocument(gratuityDocument.freePass);
-					this.applyGratuity();
-				}
-		else {
-			this.setGratuityType(gratuityType);
-		}
-			
+
+	public enum GratuityType {
+		noGratuity, elderly, phisicallyChallenged
 	}
 
-	public void addTicket(Ticket p) {
-		
-		if (gratuityType == gratuity.elderly ||
-			gratuityType == gratuity.phisicallyChallenged) {
-			p.setPrice(0f);
+	public enum GratuityDocument {
+		birthDate, freePass
+	}
+
+	public Client(String name, String phone, String email, String address, String cpf, GratuityType gratuityType,
+			ArrayList<Ticket> tickets) {
+		super(name, phone, email, address);
+
+		this.setCpf(cpf);
+		this.setTickets(tickets);
+
+		// checking gratuity category the client applies to
+		// and applying it to all existing tickets if so
+		if (gratuityType == GratuityType.elderly) {
+			this.setGratuityType(gratuityType);
+			this.setGratuityDocument(GratuityDocument.birthDate);
+			this.applyGratuity();
+		} else if (gratuityType == GratuityType.phisicallyChallenged) {
+			this.setGratuityType(gratuityType);
+			this.setGratuityDocument(GratuityDocument.freePass);
+			this.applyGratuity();
+		} else {
+			this.setGratuityType(gratuityType);
 		}
-		
-		tickets.add(p);
-		//System.out.println("A passagem foi adicionada com sucesso!");
-	}	 
-	
-	public void removeTicket(int i) {
-		//System.out.println("A passagem" + (i + 1) + "foi removida com sucesso!");
-		tickets.remove(i);
+
 	}
-	
-	public void updateTicket(Ticket p, int i) {
-		tickets.set(i, p);
-		//System.out.println("A passagem" + (i + 1) + "foi atualizada! com sucesso");
+
+	public Client(String name, String phone, String email, String address, String cpf, String gratuityType) {
+		super(name, phone, email, address);
+
+		setCpf(cpf);
+		setGratuity(gratuityType);
+
+		if (gratuityType == "Idade") {
+			setGratuityType(GratuityType.elderly);
+			setGratuityDocument(GratuityDocument.birthDate);
+			applyGratuity();
+		} else if (gratuityType == "Deficiência física") {
+			setGratuityType(GratuityType.phisicallyChallenged);
+			setGratuityDocument(GratuityDocument.freePass);
+			applyGratuity();
+		} else {
+			setGratuityType(GratuityType.elderly);
+		}
 	}
-	
-	private void applyGratuity () {
-		//is called only inside the constructor if the client
-		//has the gratuity right
+
+	private void applyGratuity() {
+		// is called only inside the constructor if the client
+		// has the gratuity right
 		for (int i = 0; i < this.getTickets().size(); i++) {
 			this.getTickets().get(i).setPrice(0f);
-		}
-	}
-	
-	public void listTickets() {
-		for (int i = 0; i < tickets.size(); i++) {
-			//System.out.println("Listando passagens do cliente: " + this.getName() + "\n");
-			//System.out.println("Passagem " + (i + 1) + "\n");
-			this.tickets.get(i).toString();;
-			//System.out.println("------------------------------------");
 		}
 	}
 
@@ -88,19 +75,27 @@ public class Client extends User {
 		this.cpf = cpf;
 	}
 
-	public gratuity getGratuityType() {
+	public String getGratuity() {
+		return gratuity;
+	}
+
+	public void setGratuity(String gratuity) {
+		this.gratuity = gratuity;
+	}
+
+	public GratuityType getGratuityType() {
 		return gratuityType;
 	}
 
-	public void setGratuityType(gratuity gratuityType) {
+	public void setGratuityType(GratuityType gratuityType) {
 		this.gratuityType = gratuityType;
 	}
-	
-	public gratuityDocument getGratuityDocument() {
+
+	public GratuityDocument getGratuityDocument() {
 		return gratuityDocument;
 	}
 
-	public void setGratuityDocument(gratuityDocument gratuityDocument) {
+	public void setGratuityDocument(GratuityDocument gratuityDocument) {
 		this.gratuityDocument = gratuityDocument;
 	}
 
@@ -110,14 +105,5 @@ public class Client extends User {
 
 	public void setTickets(ArrayList<Ticket> tickets) {
 		this.tickets = tickets;
-	}
-
-	public String toString() {
-		String saida;
-		
-		saida = super.toString();
-		saida = saida + "Documento (CPF): 	" + this.getCpf();
-		saida = saida + "\nNúmero de passagens: 	" + this.getTickets().size();
-		return saida;
 	}
 }

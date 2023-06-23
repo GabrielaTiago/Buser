05ff19b037391b73cb8d8bf4436f80bfb76c3f44
gabri <gabri@DESKTOP-03L0QEE>
2023-06-23 00:22:42 -0300

    implemented the method to intialize a JTable

--- Buser/src/view/TicketsTable.java
+++ Buser/src/view/TicketsTable.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JTable;
 
+import controllers.DatabaseController;
+import controllers.TicketController;
 import database.Database;
 import models.*;
 
@@ -14,15 +16,60 @@ public class TicketsTable{
 	
 	private Database d;
 	private JTable table;
-	private String tableColumns[] = {"Preço","Tipo de Poltrona","Número Poltrona","Itinerário"};
-	private String tableData[][] = {{""}, {""}, {""}, {""}};
+	private String tableColumns[] = {"ID", "Preço","Tipo de Poltrona","Número Poltrona","Itinerário"};
+	private String tableData[][];
 	
-	public TicketsTable(ArrayList<Ticket> tickets) {
+	public TicketsTable(DatabaseController controller, String[] iti) {
+		int existingTickets = controller.getTicketController().getTicketsQt();
+		int scenario;
+		tableData = new String[15][5]; 
 		
+		if (existingTickets > 0) {
+		//there are tickets in the database instance and we must get their values
+			scenario = 1;
+		}
+		else {
+		//there are no tickets in the database and we can initialize with empty data
+			scenario = 2;
+		}
+		
+		for (int i = 0; i < 7; i++) {
+			
+			switch(scenario) {
+				case (1):
+					Ticket t = controller.getData().getTickets().get(i);
+					//saves the ticket i, in database tickets,
+					//data in a string array to fill the cell at the table 
+					String[] values = {String.valueOf(t.getId()),
+									   String.valueOf(t.getPrice()),
+									   t.getSeatType().toString(),
+									   t.getSeatNumber(),
+									   iti[1]};
+				
+					if (i < existingTickets) {
+						for(int j = 0; j < 4; j++) {
+							tableData[i][j] = values[j];
+						}
+					}
+					else {
+						for(int j = 0; j < 5; j++) {
+							tableData[i][j] = "";
+						}
+					}
+					break;
+
+				case (2): {
+					for(int j = 0; j < 5; j++) {
+						tableData[i][j] = "";
+					}
+				}
+			}
+		}
+		//creates the table with the given arrays
+		table = new JTable(tableData, tableColumns);
 	}
 	
 	public JTable getTable() {
-		return null;
+		return this.table;
 	}
-	
 }

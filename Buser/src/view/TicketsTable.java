1356b5c5a78b4e0a08f2ea6b269ed960599b8674
Gabriel <Gabriel@LAPTOP-P2KJ6ET1>
2023-06-23 18:39:54 -0300

    fixed the fillTableData() method

--- Buser/src/view/TicketsTable.java
+++ Buser/src/view/TicketsTable.java
@@ -14,40 +14,52 @@ import models.*;
 
 public class TicketsTable{
 	
-	private Database d;
+	//JComponents
+	//private JFrame window;
 	private JTable table;
 	private String tableColumns[] = {"ID", "Preço","Tipo de Poltrona","Número Poltrona","Itinerário"};
 	private String tableData[][];
+	private String[] itineraries;
 	
-	public TicketsTable(DatabaseController controller, String[] iti) {
+	public TicketsTable(DatabaseController controller) {
+		//creates the table with the given arrays
+		this.setItineraries(controller.itineraryListToString());
+		table = new JTable(fillTableData(controller, itineraries), tableColumns);
+	}
+	
+	public String[][] fillTableData(DatabaseController controller, String[] iti) {
 		int existingTickets = controller.getTicketController().getTicketsQt();
 		int scenario;
-		tableData = new String[15][5]; 
+		int lines = 30;
+		tableData = new String[lines][5]; 
 		
 		if (existingTickets > 0) {
 		//there are tickets in the database instance and we must get their values
+		//and once they are all filled in, proceed to fill with empty data up to line value
 			scenario = 1;
 		}
 		else {
-		//there are no tickets in the database and we can initialize with empty data
+		//there are no tickets in the database and we can initialize all lines with empty data
 			scenario = 2;
 		}
 		
-		for (int i = 0; i < 7; i++) {
+		for (int i = 0; i < lines - 1; i++) {
 			
 			switch(scenario) {
 				case (1):
-					Ticket t = controller.getData().getTickets().get(i);
-					//saves the ticket i, in database tickets,
-					//data in a string array to fill the cell at the table 
-					String[] values = {String.valueOf(t.getId()),
-									   String.valueOf(t.getPrice()),
-									   t.getSeatType().toString(),
-									   t.getSeatNumber(),
-									   iti[1]};
+
 				
 					if (i < existingTickets) {
-						for(int j = 0; j < 4; j++) {
+						Ticket t = controller.getData().getTickets().get(i);
+						//saves the ticket i, in database tickets,
+						//data in a string array to fill the cell at the table 
+						String[] values = {String.valueOf(t.getId()),
+										   String.valueOf(t.getPrice()),
+										   t.getSeatType().toString(),
+										   t.getSeatNumber(),
+										   iti[i]};
+						
+						for(int j = 0; j < 5; j++) {
 							tableData[i][j] = values[j];
 						}
 					}
@@ -65,11 +77,28 @@ public class TicketsTable{
 				}
 			}
 		}
-		//creates the table with the given arrays
-		table = new JTable(tableData, tableColumns);
+		return tableData;
 	}
-	
 	public JTable getTable() {
 		return this.table;
 	}
+	public String[] getTableColumns() {
+		return tableColumns;
+	}
+	public void setTableColumns(String[] tableColumns) {
+		this.tableColumns = tableColumns;
+	}
+	public String[][] getTableData() {
+		return tableData;
+	}
+	public void setTableData(String[][] tableData) {
+		this.tableData = tableData;
+	}
+	public String[] getItineraries() {
+		return itineraries;
+	}
+	public void setItineraries(String[] itineraries) {
+		this.itineraries = itineraries;
+	}
+	
 }

102833fe44d077deaf9b0534f3e6b795779ced10
gabri <gabri@DESKTOP-03L0QEE>
2023-06-23 00:26:26 -0300

    atributes are now private

--- Buser/src/controllers/DatabaseController.java
+++ Buser/src/controllers/DatabaseController.java
@@ -6,26 +6,33 @@ import database.Database;
 import models.Itinerary;
 
 public class DatabaseController {
-	Database data = new Database();
-	TicketController ticketController = new TicketController(this);
+	private Database data = new Database();
+	private TicketController ticketController = new TicketController(this);
 	
+	//mover para itinerary controller
+	//begin
 	public String[] itineraryListToString() {
 		int size = this.getData().getItineraries().size();
 		String[] itinerariesStrings = new String[size];
+		
 		for (int i = 0; i < size; i++) {
 			String stringValue;
+			
 			stringValue = this.getData().getItineraries().get(i).getOrigin() + " " +
 						  this.getData().getItineraries().get(i).getDepartureDate() + " - " +
 						  this.getData().getItineraries().get(i).getDestination() + " " +
 						  this.getData().getItineraries().get(i).getArrivalDate();
+			
 			itinerariesStrings[i] = stringValue;
 		}
+		
 		return itinerariesStrings;
 	}
 
 	public ArrayList<Itinerary> getItineraries() {
 		return data.getItineraries();
 	}
+	//end
 
 	public Database getData() {
 		return data;

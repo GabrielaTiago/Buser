8bc7138343083db0f07ddea5d9daaddaa51dae89
Gabriel <Gabriel@172.29.112.205>
2023-06-24 18:30:44 -0300

    ticketsTable is now a separate window from TicketWindow

--- Buser/src/view/TicketsTable.java
+++ Buser/src/view/TicketsTable.java
@@ -4,31 +4,97 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-import controllers.DatabaseController;
 import controllers.TicketController;
 import database.Database;
 import models.*;
 
-public class TicketsTable{
+public class TicketsTable implements ActionListener{
 	
 	//JComponents
-	//private JFrame window;
-	private JTable table;
-	private String tableColumns[] = {"ID", "Preço","Tipo de Poltrona","Número Poltrona","Itinerário"};
+	private JFrame window;
+	private JTable ticketsTable;
+	private JScrollPane tablePane;
+	private JLabel indexLabel;
+	private JButton editButton, deleteButton, exitButton;
+	private JComboBox indexList;
+	private String tableColumns[] = {"Index", "Preço","Tipo de Poltrona","Número Poltrona","Itinerário"};
 	private String tableData[][];
 	private String[] itineraries;
+	private TicketController controller;
 	
-	public TicketsTable(DatabaseController controller) {
+	public TicketsTable(TicketController controller) {
 		//creates the table with the given arrays
-		this.setItineraries(controller.itineraryListToString());
-		table = new JTable(fillTableData(controller, itineraries), tableColumns);
+		this.controller = controller;
+		this.setItineraries(controller.itineraryListToString(1));
+		
+		//JFrame settings
+		window = new JFrame("Lista de Passagens");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(850, 470);
+		window.setLayout(null);
+		
+		//JTable
+		ticketsTable = new JTable(fillTableData(controller, itineraries), tableColumns); 
+		ticketsTable.getColumnModel().getColumn(4).setWidth(400);
+		tablePane = new JScrollPane(ticketsTable);
+		
+		indexList = new JComboBox<String>(controller.getIndexes());
+		
+		indexLabel = new JLabel("Index:");
+		
+		editButton = new JButton("Editar");
+		deleteButton = new JButton("Deletar");
+		exitButton = new JButton("Voltar");
+		
+		editButton.addActionListener(this);
+		deleteButton.addActionListener(this);
+		exitButton.addActionListener(this);
+		
+		int margem = 190;
+		tablePane.setBounds(20, 30, 800, 330);
+		indexLabel.setBounds(  margem,   	          		 380, 210, 30);
+		indexList.setBounds(   margem + 45,         		 380, 80, 30);
+		editButton.setBounds(  margem + 45 + 90,    		 380, 100, 30);
+		deleteButton.setBounds(margem + 45 + 90 + 110, 		 380, 100, 30);
+		exitButton.setBounds(  margem + 45 + 90 + 110 + 110, 380, 100, 30);
+		
+		window.add(indexList);
+		window.add(indexLabel);
+		window.add(deleteButton);
+		window.add(editButton);
+		window.add(tablePane);
+		window.add(exitButton);
+		
+		window.setVisible(true);
 	}
 	
-	public String[][] fillTableData(DatabaseController controller, String[] iti) {
-		int existingTickets = controller.getTicketController().getTicketsQt();
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getActionCommand().equals("Editar")) {
+			int i = indexList.getSelectedIndex();
+			controller.setToUpdateValues(i);
+			window.dispose();
+			TicketWindow a = new TicketWindow(controller, 1);
+		}
+		if (ae.getActionCommand().equals("Deletar")) {
+			int i = indexList.getSelectedIndex();
+			controller.deleteTicket(i);;
+			window.dispose();
+		}
+		if (ae.getActionCommand().equals("Voltar")) {
+			window.dispose();
+		}
+	}
+	
+	public String[][] fillTableData(TicketController controller, String[] itineraries) {
+		//returns returns the tabledata in a matrix form 
+		int existingTickets = controller.getTicketsQt();
 		int scenario;
 		int lines = 30;
 		tableData = new String[lines][5]; 
@@ -43,21 +109,21 @@ public class TicketsTable{
 			scenario = 2;
 		}
 		
-		for (int i = 0; i < lines - 1; i++) {
+		for (int i = 0; i < lines; i++) {
 			
 			switch(scenario) {
 				case (1):
 
 				
 					if (i < existingTickets) {
-						Ticket t = controller.getData().getTickets().get(i);
+						Ticket t = controller.getTickets().get(i);
 						//saves the ticket i, in database tickets,
 						//data in a string array to fill the cell at the table 
-						String[] values = {String.valueOf(t.getId()),
+						String[] values = {String.valueOf(i),
 										   String.valueOf(t.getPrice()),
 										   t.getSeatType().toString(),
 										   t.getSeatNumber(),
-										   iti[i]};
+										   itineraries[i]};
 						
 						for(int j = 0; j < 5; j++) {
 							tableData[i][j] = values[j];
@@ -79,9 +145,7 @@ public class TicketsTable{
 		}
 		return tableData;
 	}
-	public JTable getTable() {
-		return this.table;
-	}
+
 	public String[] getTableColumns() {
 		return tableColumns;
 	}
@@ -100,5 +164,4 @@ public class TicketsTable{
 	public void setItineraries(String[] itineraries) {
 		this.itineraries = itineraries;
 	}
-	
 }

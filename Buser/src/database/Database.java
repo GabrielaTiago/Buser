dfc3637dd37dea540c56dec3ed3c14fd16596895
Gabriel <Gabriel@LAPTOP-P2KJ6ET1>
2023-06-28 09:47:47 -0300

    replaced tickets ArrayList<Ticket> for Client atribute

--- Buser/src/database/Database.java
+++ Buser/src/database/Database.java
@@ -5,40 +5,9 @@ import java.util.ArrayList;
 import models.Client;
 import models.Company;
 import models.Itinerary;
-import models.Ticket;
-import models.Ticket.SeatType;
 
 public abstract class Database {
-	private Client client;
-	private Company company;
-	private static final ArrayList<Itinerary> itineraries = new ArrayList<Itinerary>();
-	private static final ArrayList<Ticket> tickets = new ArrayList<Ticket>();
-	
-	public void addTicket(Ticket t) {
-		Database.getTickets().add(t);
-	}
-	public void removeTicket(int i) {
-		Database.getTickets().remove(i);
-	}
-	public void updateTicket(Ticket t, int i) {
-		Database.getTickets().set(i, t);
-	}
-
-	
-	//getters and setters
-	public Client getClient() {
-		return client;
-	}
-	
-	public void getCompany(Company company) {
-		this.company = company;
-	}
-
-	public static ArrayList<Itinerary> getItineraries() {
-		return itineraries;
-	}
-
-	public static ArrayList<Ticket> getTickets() {
-		return tickets;
-	}
+	public static Client client;
+	public static Company company;
+	public static final ArrayList<Itinerary> itineraries = new ArrayList<Itinerary>();
 }

16205b059aa744e580283eeebe8ec6d945cab00f
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-07-01 12:46:26 -0300

    Adds password field to the registration screen

--- Buser/src/view/CompanyAuthScreen.java
+++ Buser/src/view/CompanyAuthScreen.java
@@ -14,26 +14,33 @@ public class CompanyAuthScreen implements ActionListener {
 	private static JLabel title = new JLabel("Boas-vindas à Buser!");
 	private static JTextField nameField = new JTextField();
 	private static JTextField emailField = new JTextField();
+	private static JTextField passwordField = new JPasswordField();
 	private static JTextField phoneField = new JTextField();
 	private static JTextField addressField = new JTextField();
 	private static JTextField cnpjField = new JTextField();
 	private static JTextField corporateNameField = new JTextField();
 	private static JButton registerButton = new JButton();
+	private static JButton loginButton = new JButton();
 	private static JButton linkTo = new JButton();
 
 	public CompanyAuthScreen() {
 		title.setFont(new Font("Serif", Font.BOLD, 36));
 		title.setHorizontalAlignment(SwingConstants.CENTER);
 
-		JPanel contentPane = new JPanel(new GridBagLayout());
+		JPanel container = new JPanel();
+		container.setLayout(new BorderLayout());
+		container.setBorder(BorderFactory.createEmptyBorder(55, 55, 55, 55));
+
+		JPanel authContainer = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.insets = new Insets(10, 10, 10, 10);
+		gbc.insets = new Insets(8, 25, 8, 25);
+		authContainer.setBackground(new Color(250, 250, 250));
 
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.gridwidth = 2;
 		gbc.anchor = GridBagConstraints.CENTER;
-		contentPane.add(title, gbc);
+		authContainer.add(title, gbc);
 
 		gbc.gridy = 1;
 		gbc.gridwidth = 1;
@@ -43,40 +50,46 @@ public class CompanyAuthScreen implements ActionListener {
 		gbc.gridy = 1;
 		gbc.weightx = 1.0;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		contentPane.add(textField(nameField, "Nome"), gbc);
+		authContainer.add(textField(nameField, "Nome"), gbc);
 
 		gbc.gridy = 2;
-		contentPane.add(textField(emailField, "Email"), gbc);
+		authContainer.add(textField(emailField, "Email"), gbc);
 
 		gbc.gridy = 3;
-		contentPane.add(textField(phoneField, "Telefone"), gbc);
+		authContainer.add(passwordField((JPasswordField) passwordField, "Senha"), gbc);
 
 		gbc.gridy = 4;
-		contentPane.add(textField(addressField, "Endereço"), gbc);
+		authContainer.add(textField(phoneField, "Telefone"), gbc);
 
 		gbc.gridy = 5;
-		contentPane.add(textField(cnpjField, "CNPJ"), gbc);
+		authContainer.add(textField(addressField, "Endereço"), gbc);
 
 		gbc.gridy = 6;
-		contentPane.add(textField(corporateNameField, "Razão Social"), gbc);
+		authContainer.add(textField(cnpjField, "CNPJ"), gbc);
+
+		gbc.gridy = 7;
+		authContainer.add(textField(corporateNameField, "Razão Social"), gbc);
 
 		gbc.gridx = 0;
-		gbc.gridy = 7;
-		gbc.gridwidth = 2;
-		gbc.anchor = GridBagConstraints.CENTER;
-		gbc.insets = new Insets(30, 10, 10, 10);
-		contentPane.add(button(registerButton, "Cadastrar"), gbc);
-
 		gbc.gridy = 8;
 		gbc.gridwidth = 2;
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.insets = new Insets(30, 25, 10, 25);
+		authContainer.add(button(registerButton, "Cadastrar"), gbc);
+
+		gbc.gridy = 9;
+		gbc.gridwidth = 2;
 		gbc.fill = GridBagConstraints.NONE;
-		gbc.insets = new Insets(10, 10, 10, 10);
-		contentPane.add(linkButton(linkTo, "Cadastro de Cliente"), gbc);
+		gbc.insets = new Insets(10, 25, 10, 25);
+		authContainer.add(linkButton(loginButton, "Já possui cadastro como empresa? Clique aqui para entrar"), gbc);
 
-		window.setContentPane(contentPane);
+		gbc.gridy = 10;
+		authContainer.add(linkButton(linkTo, "Cadastro de Cliente"), gbc);
 
-		window.setSize(800, 600);
-		window.getContentPane().setBackground(new Color(250, 250, 250));
+		container.add(authContainer, BorderLayout.CENTER);
+
+		window.setContentPane(container);
+		window.setSize(800, 700);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
@@ -89,7 +102,6 @@ public class CompanyAuthScreen implements ActionListener {
 	private JTextField textField(JTextField textField, String placeholder) {
 		textField.setOpaque(false);
 		textField.setPreferredSize(new Dimension(textField.getPreferredSize().width, 30));
-
 		textField.setForeground(new Color(117, 117, 138));
 		textField.setText(placeholder);
 
@@ -138,6 +150,36 @@ public class CompanyAuthScreen implements ActionListener {
 		return button;
 	}
 
+	private JPasswordField passwordField(JPasswordField passwordField, String placeholder) {
+		passwordField.setOpaque(false);
+		passwordField.setPreferredSize(new Dimension(passwordField.getPreferredSize().width, 30));
+		passwordField.setForeground(new Color(117, 117, 138));
+		passwordField.setText(placeholder);
+		passwordField.setEchoChar((char) 0);
+
+		passwordField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent event) {
+				if (String.valueOf(passwordField.getPassword()).equals(placeholder)) {
+					passwordField.setText("");
+					passwordField.setEchoChar('\u2022');
+					passwordField.setForeground(Color.BLACK);
+				}
+			}
+
+			@Override
+			public void focusLost(FocusEvent event) {
+				if (String.valueOf(passwordField.getPassword()).isEmpty()) {
+					passwordField.setEchoChar((char) 0);
+					passwordField.setForeground(new Color(117, 117, 138));
+					passwordField.setText(placeholder);
+				}
+			}
+		});
+
+		return passwordField;
+	}
+
 	private JButton linkButton(JButton linkButton, String text) {
 		linkButton.setText(text);
 		linkButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -171,12 +213,13 @@ public class CompanyAuthScreen implements ActionListener {
 		if (src == registerButton) {
 			String name = nameField.getText();
 			String email = emailField.getText();
+			String password = passwordField.getText();
 			String phone = phoneField.getText();
 			String address = addressField.getText();
 			String cnpj = cnpjField.getText();
 			String corporateName = corporateNameField.getText();
 
-			Company companyData = new Company(name, email, phone, address, cnpj, corporateName);
+			Company companyData = new Company(name, email, password, phone, address, cnpj, corporateName);
 			String errorMessage = AuthController.validatesCompanyData(companyData);
 
 			if (errorMessage.isEmpty()) {

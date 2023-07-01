a7979e31d6a7842e6dc4aefb73ad64709added81
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-07-01 17:17:32 -0300

    Adds mock data

--- Buser/src/database/Database.java
+++ Buser/src/database/Database.java
@@ -4,12 +4,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import models.*;
+import models.Client.GratuityType;
 
 public class Database {
 	private static final ArrayList<Itinerary> itineraries = new ArrayList<>();
 	private static final ArrayList<Ticket> tickets = new ArrayList<>();
-	private static final ArrayList<Client> logedClient = new ArrayList<>();
-	private static final ArrayList<Company> logedCompany = new ArrayList<>();
+	private static final ArrayList<Client> clients = new ArrayList<>();
+	private static final ArrayList<Company> companys = new ArrayList<>();
 
 	public static ArrayList<Itinerary> getItinaraiesData() {
 		return itineraries;
@@ -19,17 +20,22 @@ public class Database {
 		return tickets;
 	}
 
-	public static ArrayList<Client> getClient() {
-		return logedClient;
+	public static ArrayList<Client> getClientData() {
+		return clients;
 	}
-	
-	public static ArrayList<Company> getCompany() {
-		return logedCompany;
+
+	public static ArrayList<Company> getCompanyData() {
+		return companys;
 	}
-	
+
 	public static void teste() {
 		LocalDate d1 = LocalDate.now();
-		Company company = new Company("Nome", "email", "telefone", "endreço", "cnpj", "razao social", itineraries);
+		Company company = new Company("Araguarina", "araguarina@hotmail.com", "Teste123$", "6233445566",
+				"Avenida dos Bobos, n 0 - Lugar Nenhum - LN", "46922732000130", "Araguarina LTDA", itineraries);
+		companys.add(company);
+		Client client = new Client("Gabriela", "gabriela@gmail.com", "Teste123$", "62988776655",
+				"Avenida Brasil, n 765 - Algum Lugar AG", "50191137006", GratuityType.noGratuity, tickets);
+		clients.add(client);
 		Itinerary i1 = new Itinerary("Anápolis", "Brasília", d1, "10:00", "14:00", company);
 		Itinerary i2 = new Itinerary("Anápolis", "Goiânia", d1, "08:00", "09:00", company);
 		Itinerary i3 = new Itinerary("Anápolis", "Corumbá", d1, "09:00", "10:10", company);

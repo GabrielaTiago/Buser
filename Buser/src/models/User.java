da30955922fd46bfe563ced88f73373e1a61f1df
Gabriel <Gabriel@172.29.70.104>
2023-07-01 18:40:27 -0300

    Added javadoc comments

--- Buser/src/models/User.java
+++ Buser/src/models/User.java
@@ -1,11 +1,23 @@
 package models;
-
+/**
+ * Classe abstrata que representa um usuária que tem 2 casos de herança
+ * , um para Cliente e outro para Compania, ambos herdam os atributos de 
+ * Nome, telefone, email e endereço.
+ * @author Gabriel
+ *
+ */
 public abstract class User {
 	private String name;
 	private String phoneNumber;
 	private String email;
 	private String address;
-
+/**
+ * Construtor da classe user
+ * @param name
+ * @param phone
+ * @param email
+ * @param address
+ */
 	public User(String name, String phone, String email, String address) {
 		this.setName(name);
 		this.setPhoneNumber(phone);

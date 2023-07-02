6d79dfa4967c853c2adfcf9240b26659a9a656ae
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-07-02 12:02:38 -0300

    Adds javadoc in to Company model

--- Buser/src/models/Company.java
+++ Buser/src/models/Company.java
@@ -2,11 +2,31 @@ package models;
 
 import java.util.ArrayList;
 
+/**
+ * Client class, inheriting characteristics of the parent class User
+ * 
+ * @author Gabriela Tiago
+ * @since 2023
+ * @version 1.1
+ * 
+ */
 public class Company extends User {
 	private String cnpj;
 	private String corporateName;
 	private ArrayList<Itinerary> itineraries;
 
+	/**
+	 * Class constructor for instantiating a new Company
+	 * 
+	 * @param name          The company's name
+	 * @param email         The client's email
+	 * @param password      The client's password
+	 * @param phone         The client's phone
+	 * @param address       The client's address
+	 * @param cnpj          The client's cnpj document
+	 * @param corporateName The client's corporate name
+	 * @param itineraries   The company's itineraries registered
+	 */
 	public Company(String name, String email, String password, String phone, String address, String cnpj,
 			String corporateName, ArrayList<Itinerary> itineraries) {
 		super(name, email, password, phone, address);
@@ -15,6 +35,18 @@ public class Company extends User {
 		this.itineraries = itineraries;
 	}
 
+	/**
+	 * Class constructor for instantiating a new Company to perform the necessary
+	 * validations
+	 * 
+	 * @param name          The company's name
+	 * @param email         The client's email
+	 * @param password      The client's password
+	 * @param phone         The client's phone
+	 * @param address       The client's address
+	 * @param cnpj          The client's cnpj document
+	 * @param corporateName The client's corporate name
+	 */
 	public Company(String name, String email, String password, String phone, String address, String cnpj,
 			String corporateName) {
 		super(name, email, password, phone, address);

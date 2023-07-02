9a823ba71ad771be98174aba0e8ecaf20858b716
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-07-02 16:00:13 -0300

    Adds javadocs comments in the WrapperContainer

--- Buser/src/view/WrapperContainer.java
+++ Buser/src/view/WrapperContainer.java
@@ -3,18 +3,38 @@ package view;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Represents a custom JPanel that serves as a container for other components.
+ * It provides methods for adding components to the container using a
+ * GridBagLayout.
+ * 
+ * @author Gabriela Tiago
+ * @since 2023
+ * @version 1.1
+ * 
+ */
 public class WrapperContainer extends JPanel {
 	/**
-	 * 
+	 * A version number for serialization
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructs a new WrapperContainer with a GridBagLayout
+	 */
 	public WrapperContainer() {
 		setLayout(new GridBagLayout());
 		setBackground(null);
 	}
 
-	public void addComponents(JPanel itinerary) {
+	/**
+	 * 
+	 * Adds the specified JPanel to the container using the GridBagLayout
+	 *
+	 * @param component the JPanel to be added to the container
+	 * 
+	 */
+	public void addComponents(JPanel component) {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = getComponentCount();
@@ -23,6 +43,6 @@ public class WrapperContainer extends JPanel {
 		gbc.weightx = 1.0;
 		gbc.insets = new Insets(8, 0, 8, 0);
 
-		add(itinerary, gbc);
+		add(component, gbc);
 	}
 }

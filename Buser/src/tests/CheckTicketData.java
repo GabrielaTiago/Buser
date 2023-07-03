4e8e68a622504814908ffd56124e76ba59680c17
gabri <gabri@DESKTOP-03L0QEE>
2023-07-02 21:38:15 -0300

    Created a test for ticket data

--- Buser/src/tests/CheckTicketData.java
+++ Buser/src/tests/CheckTicketData.java
@@ -1,14 +1,43 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import controllers.*;
 
+/**
+ * This class is from the junitTests package and is responsible for the 3 Junit
+ * test cases necessary
+ * 
+ * @author Gabriel Fernando
+ *
+ */
 class CheckTicketData {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	/**
+	 * This test asserts the combinations that are valid and invalid for a ticket's
+	 * price and seatNumber atributes
+	 */
+	void testCheckTicketData() {
 
+		// only the value -1 is invalid
+		float[] priceValues = { -1, 0, 1 };
+
+		// only the value 1 is valid
+		int[] seatNumberValues = { -1, 0, 1 };
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (i != 0 && j == 2) {
+					// the only valid option is if both of them are valid
+					assertTrue(TicketController.checkTicketData(priceValues[i], seatNumberValues[j]));
+				} else {
+					assertFalse(TicketController.checkTicketData(priceValues[i], seatNumberValues[j]));
+				}
+			}
+		}
+	}
 }
+
